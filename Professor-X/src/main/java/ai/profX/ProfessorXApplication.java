78713df5a294dea4f0afbf0ffa6dbc9398cf6866
Abhilash S <abhilash@infrrd.ai>
2017-02-26 23:55:24 +0530

    Initial Project structure, dependecies, properties

---
+++ Professor-X/src/main/java/ai/profX/ProfessorXApplication.java
@@ -0,0 +1,12 @@
+package ai.profX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorXApplication.class, args);
+	}
+}
---
+++ Professor-X/src/test/java/ai/profX/ProfessorXApplicationTests.java
@@ -0,0 +1,16 @@
+package ai.profX;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProfessorXApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
