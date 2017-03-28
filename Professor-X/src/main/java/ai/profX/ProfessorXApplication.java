45be27dbdfbafdc63aaf46fe67b3a872d5432de2
Abhilash S <abhilash@infrrd.ai>
2017-03-28 21:40:10 +0530

    Removed HttpOnly from header for cookies

--- Professor-X/src/main/java/ai/profX/ProfessorXApplication.java
+++ Professor-X/src/main/java/ai/profX/ProfessorXApplication.java
@@ -1,13 +1,19 @@
 package ai.profX;
 
+import org.apache.catalina.Context;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableCaching
@@ -40,5 +46,19 @@ public class ProfessorXApplication {
 		cmfb.setConfigLocation(new ClassPathResource("ehcache.xml"));
 		cmfb.setShared(true);
 		return cmfb;
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+		factory.setTomcatContextCustomizers(Arrays.asList(new CustomCustomizer()));
+		return factory;
+	}
+
+	static class CustomCustomizer implements TomcatContextCustomizer {
+		@Override
+		public void customize(Context context) {
+			context.setUseHttpOnly(false);
+		}
 	}
 }
